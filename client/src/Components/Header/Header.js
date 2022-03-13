89ee3d7b19c445d45720e9f6b575148f530d1551
Ana <analucia.spi02@gmail.com>
2022-03-13 16:11:07 -0400

    add components

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+    <p className='header__title'>Kumojin Project</p>
+  </header>
+  )
+}
+
+export default Header
