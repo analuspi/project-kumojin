964b766ebd9fc5dbebc702ded8deca882e0ad1aa
Ana <analucia.spi02@gmail.com>
2022-03-16 06:36:53 -0400

    finish api

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/Header";
 import NewEvent from "./Components/Events/NewEvent";
 import EventList from "./Components/Events/EventList";
@@ -7,23 +7,57 @@ import "./App.css";
 function App() {
   const [loadedEvents, setLoadedEvents] = useState([]);
 
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch("http://localhost:5000/events");
+      const responseData = await response.json();
+
+      setLoadedEvents(responseData.events);
+    };
+    fetchEvents();
+  }, []);
+
   const addEventHandler = async (
     eventName,
     eventDescription,
     eventStartDate,
     eventEndDate
   ) => {
-    const newEvent = {
-      title: eventName,
-      description: eventDescription,
-      startDate: eventStartDate,
-      endDate: eventEndDate
-    };
-    setLoadedEvents((prevEvents) => {
-      return prevEvents.concat({
-        ...newEvent,
+    try {
+      const newEvent = {
+        title: eventName,
+        description: eventDescription,
+        startDate: eventStartDate,
+        endDate: eventEndDate
+      };
+      let hasError = false;
+      const response = await fetch("http://localhost:5000/event", {
+        method: "POST",
+        body: JSON.stringify(newEvent),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
-    });
+
+      if (!response.ok) {
+        hasError = true;
+      }
+
+      const responseData = await response.json();
+
+      if (hasError) {
+        throw new Error(responseData.message);
+      }
+
+      setLoadedEvents((prevEvents) => {
+        return prevEvents.concat({
+          ...newEvent,
+          id: responseData.event.id,
+        });
+      });
+    } catch (error) {
+      alert(error.message || "Something went wrong!");
+    }
   };
 
   return (
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
@@ -7,6 +8,38 @@ const DUMMY_EVENTS = [];
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
 
+app.get("/events", (req, res, next) => {
+  res.status(200).json({ events: DUMMY_EVENTS });
+});
+
+app.post("/event", (req, res, next) => {
+  const { title, description, startDate, endDate } = req.body;
+
+  const createdEvent = {
+   title,
+   description,
+   startDate,
+   endDate
+  };
+
+  DUMMY_EVENTS.push(createdEvent);
+
+  res
+  .status(201)
+  .json({ message: "Created new event", event: createdEvent });
+});
 
 app.listen(5000);
